78dd1edb13c079d0b5d08e4e042376cf758e790c
Samuel Friese <samuel.friese@tum.de>
2024-01-19 22:12:47 +0100

    m

--- playground.py
+++ playground.py
@@ -33,7 +33,7 @@ gen = LatexProducer(model, vocab, max_len=150)
 for imgs, formulas in dataset:
     print("_______________________________________________________________________________________________")
     gen._print_target_sequence(tf.squeeze(formulas).numpy())
-    print("beam_search: ", gen._beam_search(imgs))
+    print("beam_search: ", gen._beam_search(imgs, beam_width=10))
     print("greedy_search: ", gen._greedy_decoding(imgs))
     print("_______________________________________________________________________________________________")
 
