f4a8512dbb29f23bbfb3f68b5b74977035dfecb0
iqwertz <juliushussl@gmail.com>
2024-01-26 17:46:46 +0100

    added more testing

--- simple_website_test/index.js
+++ simple_website_test/index.js
@@ -1,17 +1,26 @@
 const tf = require("@tensorflow/tfjs");
 const tfn = require("@tensorflow/tfjs-node");
+const fs = require("fs");
 
 async function predict() {
   const MODEL_URL = "data/model.json";
   const handler = tfn.io.fileSystem(MODEL_URL);
 
   const model = await tf.loadGraphModel(handler);
-  let tensor = tf.zeros([1, 96, 480, 1]);
-  let prediction = model.predictAsync(tensor);
-  prediction.then((res) => {
-    let data = res.dataSync();
-    console.log(data);
-  });
+  let tensor = loadImageTensor("1.png");
+  let prediction = await model.predictAsync(tensor);
+  console.log(prediction);
+  let probabilities = softmax(predictions, -1);
+}
+
+function loadImageTensor(path) {
+  const imageBuffer = fs.readFileSync(path);
+  let tfimage = tfn.node.decodeImage(imageBuffer, 1); //default #channel 4
+  //reshape
+  tfimage = tfimage.reshape([1, 96, 480, 1]);
+  tfimage = tfimage.cast("float32");
+  console.log(tfimage);
+  return tfimage;
 }
 
 predict();
